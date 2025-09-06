a00413c86141beaaad1de5c435d4e26d374511e6
Max Dexheimer <maxdexh03@gmail.com>
2025-09-06 20:49:36 +0200

    Full rewrite of the ops test runner

--- src/ops/testing.rs
+++ src/ops/testing.rs
@@ -1,11 +1,5 @@
 #![cfg(test)]
 
-use crate::{Uint, uint};
-
-pub(crate) type SatDec<N> = uint::From<crate::ops::satdec::SatDecIfL<N>>;
-pub(crate) const DEFAULT_LO: u128 = 0;
-pub(crate) type DefaultHi = crate::consts::_10;
-
 #[test]
 /// Make sure the test runner is actually testing anything, since it uses SatDec to traverse ranges.
 fn test_satdec() {
@@ -23,6 +17,100 @@ fn test_satdec() {
     tests! { 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 }
 }
 
+use crate::{
+    Uint,
+    consts::{_0, _1},
+    uint,
+};
+
+pub(crate) type SatDec<N> = uint::From<crate::ops::satdec::SatDecIfL<N>>;
+pub(crate) type DefaultHi = crate::consts::_10;
+pub(crate) type DefaultLo = crate::consts::_0;
+
+pub trait UintList {
+    type IsEmpty: Uint;
+
+    type First: Uint;
+    type Rest: UintList;
+}
+impl UintList for () {
+    type IsEmpty = _1;
+
+    type First = _0;
+    type Rest = Self;
+}
+impl<N: Uint, L: UintList> UintList for (N, L) {
+    type IsEmpty = _0;
+
+    type First = N;
+    type Rest = L;
+}
+pub trait UintRanges {
+    const EMPTY: bool;
+    type FirstLo: Uint;
+    type FirstHi: Uint;
+    type Rest: UintRanges;
+
+    type ReduceLeaf<L: TestLeaf>: TestLeaf;
+}
+impl UintRanges for () {
+    const EMPTY: bool = true;
+    type FirstLo = _0;
+    type FirstHi = _0;
+    type Rest = Self;
+    type ReduceLeaf<T: TestLeaf> = T;
+}
+impl<L: Uint, H: Uint, R: UintRanges> UintRanges for ((L, H), R) {
+    const EMPTY: bool = false;
+    type FirstLo = L;
+    type FirstHi = H;
+    type Rest = R;
+    type ReduceLeaf<T: TestLeaf> = ReducedLeaf<T>;
+}
+pub struct ReducedLeaf<T>(T);
+impl<T: TestLeaf> TestLeaf for ReducedLeaf<T> {
+    type Ranges = <T::Ranges as UintRanges>::Rest;
+    fn leaf_test<L: UintList>() {
+        fn traverse<T: TestLeaf, L: UintList, N: Uint>() {
+            T::leaf_test::<(N, L)>();
+            let next = const {
+                match uint::cmp::<N, <T::Ranges as UintRanges>::FirstLo>() {
+                    core::cmp::Ordering::Less => unreachable!(),
+                    core::cmp::Ordering::Equal => || {},
+                    core::cmp::Ordering::Greater => {
+                        const fn next_traverse<T: TestLeaf, L: UintList, N: Uint>() -> fn() {
+                            traverse::<T, L, SatDec<N>>
+                        }
+                        next_traverse::<T, L, N>()
+                    }
+                }
+            };
+            next()
+        }
+        traverse::<T, L, <T::Ranges as UintRanges>::FirstHi>()
+    }
+}
+pub trait TestLeaf {
+    type Ranges: UintRanges;
+    fn leaf_test<L: UintList>();
+}
+pub fn run_test_leaf<L: TestLeaf>() {
+    let reduced = const {
+        if L::Ranges::EMPTY {
+            const fn get_leaf<L: TestLeaf>() -> fn() {
+                L::leaf_test::<()>
+            }
+            get_leaf::<L>()
+        } else {
+            const fn reduce_leaf<L: TestLeaf>() -> fn() {
+                run_test_leaf::<<L::Ranges as UintRanges>::ReduceLeaf<L>>
+            }
+            reduce_leaf::<L>()
+        }
+    };
+    reduced()
+}
+
 /// ```rust_analyzer_brace_infer
 /// test_op! {}
 /// ```
@@ -32,124 +120,106 @@ macro_rules! test_op {
         $first:ident $($param:ident)*,
         $got:ty,
         $expect:expr
-        $(,$(
-            $($lo:literal)?..$(=$hi:literal)?
-            $($rest:tt)*
-        )?)?
+        $(, $($lo:literal)?..$(=$hi:literal)?)* $(,)?
+    ) => {
+        crate::ops::testing::test_op! {
+            @shift
+            $name
+            [$first $($param)*],
+            [$($param)* Extra],
+            $got,
+            $expect,
+            [$($($lo)?..$(=$hi)?),*]
+        }
+    };
+    (
+        @shift
+        $name:ident
+        [$first:ident $($param:ident)*],
+        [$fshifted:ident $($shifted:ident)*],
+        $got:ty,
+        $expect:expr,
+        [$($($lo:literal)?..$(=$hi:literal)?),*]
     ) => {
         #[test]
         fn $name() {
-            struct __Callback<$($param),*>($($param),*);
-            impl<$($param: crate::Uint),*> crate::ops::testing::Tests for __Callback<$($param),*> {
-                type Hi = crate::ops::testing::__hi_or_default![ $($($( $hi )?)?)? ];
-                const LO: u128 = crate::ops::testing::__lo_or_default![ $($($( $lo )?)?)? ];
-                type Partial<$first: crate::Uint> = Self;
-
-                const IS_LEAF: bool = true;
-
-                #[expect(non_snake_case)]
-                fn leaf_test<$first: crate::Uint>() {
-                    let $first = crate::uint::to_u128::<$first>().unwrap();
-                    $(let $param = crate::uint::to_u128::<$param>().unwrap();)*
-                    assert_eq!(
-                        crate::uint::to_u128::<$got>(),
-                        Some($expect),
-                        "params={:?}",
-                        ($($param),*)
-                    );
+            struct Leaf;
+            impl crate::ops::testing::TestLeaf for Leaf {
+                type Ranges = crate::ops::testing::make_ranges!(
+                    [ $first $($param)* ]
+                    $(( $($lo)?, $($hi)? ))*
+                );
+                fn leaf_test<L: crate::ops::testing::UintList>() {
+                    Flattener::<L>::doit()
                 }
             }
-            crate::ops::testing::__test_op_inner! {
-                $($param)*,
-                __Callback
-                $($($($rest)*)?)?
+            struct Flattener<L>(L);
+            impl<
+                $first: crate::ops::testing::UintList<
+                    Rest = $fshifted
+                >
+                $(, $param: crate::ops::testing::UintList<
+                    Rest = $shifted
+                >)*
+                , Extra: crate::ops::testing::UintList
+            > Flattener<$first> {
+                fn doit() {
+                    doit::<$first::First $(, $param::First)*>()
+                }
             }
+            #[expect(non_snake_case)]
+            fn doit<$first: crate::Uint $(, $param: crate::Uint)*>() {
+                let $first = crate::uint::to_u128::<$first>().unwrap();
+                $(let $param = crate::uint::to_u128::<$param>().unwrap();)*
+                assert_eq!(
+                    crate::uint::to_u128::<$got>(),
+                    Some($expect),
+                    "params={:?}",
+                    ($($param),*)
+                );
+            }
+
+            crate::ops::testing::run_test_leaf::<Leaf>()
         }
-    };
+    }
 }
 pub(crate) use test_op;
-
-// TODO: optimize
-macro_rules! __test_op_inner {
+macro_rules! make_ranges {
     (
-        $first:ident $($param:ident)*,
-        $callback:ident
-        $(,$(
-            $($lo:literal)?..$(=$hi:literal)?
-            $($rest:tt)*
-        )?)?
-    ) => {{
-        struct $first<$($param),*>($($param),*);
-        impl<$($param: crate::Uint),*> crate::ops::testing::Tests for $first<$($param),*> {
-            type Hi = crate::ops::testing::__hi_or_default![ $($($( $hi )?)?)? ];
-            const LO: u128 = crate::ops::testing::__lo_or_default![ $($($( $lo )?)?)? ];
-            type Partial<$first: crate::Uint> = $callback<$first, $($param),*>;
-        }
-        crate::ops::testing::__test_op_inner! {
-            $($param)*,
-            $first
-            $($($($rest)*)?)?
-        }
-    }};
+        []
+    ) => {
+        ()
+    };
     (
-        ,
-        $callback:ident $(,)?
-    ) => {{
-        crate::ops::testing::run_tests::<$callback>()
-    }}
-}
-pub(crate) use __test_op_inner;
-
-macro_rules! __hi_or_default {
-    ($hi:expr) => {
-        crate::uint::FromU128<{ $hi }>
+        []
+        $($rest:tt)+
+    ) => {
+        core::compile_error! { "Leftover ranges" }
     };
-    () => {
-        crate::ops::testing::DefaultHi
-    }
-}
-pub(crate) use __hi_or_default;
-macro_rules! __lo_or_default {
-    ($lo:expr) => {
-        $lo
+    (
+        [ $_:ident $($rest:ident)* ]
+        $( ($($lo:expr)?, $($hi:expr)?)  $($rest2:tt)* )?
+    ) => {
+        (
+            (
+                crate::ops::testing::make_ranges!(@bound DefaultLo $($($lo)?)? ),
+                crate::ops::testing::make_ranges!(@bound DefaultHi $($($hi)?)? ),
+            ),
+            crate::ops::testing::make_ranges!([$($rest)*] $($($rest2)*)?),
+        )
     };
-    () => {
-        crate::ops::testing::DEFAULT_LO
+    (
+        @bound
+        $default:ident
+    ) => {
+        crate::ops::testing::$default
+    };
+    (
+        @bound
+        $default:ident
+        $ex:expr
+    ) => {
+        crate::uint::FromU128<{$ex}>
     };
 }
-pub(crate) use __lo_or_default;
-
-pub trait Tests {
-    type Hi: Uint;
-    const LO: u128;
-    type Partial<N: Uint>: Tests;
-
-    const IS_LEAF: bool = false;
-    fn leaf_test<N: Uint>() {}
-}
-
-pub fn run_tests<T: Tests>() {
-    fn traverse<T: Tests, N: Uint>() {
-        T::leaf_test::<N>();
-        if !T::IS_LEAF {
-            run_tests::<T::Partial<N>>()
-        }
-
-        (const {
-            let n = uint::to_u128::<N>().unwrap();
-            let lo = T::LO;
-
-            if n > lo {
-                const fn next_traverse<T: Tests, N: Uint>() -> fn() {
-                    traverse::<T, SatDec<N>>
-                }
-                next_traverse::<T, N>()
-            } else if n == lo {
-                || {}
-            } else {
-                panic!("Unnecessary monomorphization")
-            }
-        })();
-    }
-    traverse::<T, T::Hi>();
-}
+pub(crate) use make_ranges;
